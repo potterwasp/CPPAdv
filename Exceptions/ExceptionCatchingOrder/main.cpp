cacbee9761ff9f6d79396808847d8be3c7f29fc3
rohit_sinha <rohitk.in@gmail.com>
2023-06-29 07:05:03 +0530

    Exception catching order

---
+++ Exceptions/ExceptionCatchingOrder/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void FuncCanThrow()
+{
+    bool error1 = true;
+    bool error2 = false;
+
+    if(error1)
+    {
+        throw bad_alloc();
+    }
+    if(error2)
+    {
+        throw exception();
+    }
+
+}
+
+
+
+
+int main(int charC , char ** argV)
+{
+    try{
+        FuncCanThrow();
+    }
+    catch(exception e){
+        cout<<"Exception Happened : "<<e.what()<<endl;
+
+    }
+    catch(bad_alloc& e)///this will never be caught as std exception will be caught befor this 
+    {
+        cout<<"Exception Happened :"<<e.what()<<endl;
+    }
+}
